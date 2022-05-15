575efd5aa43c05c7a42603d7567276742f895596
Yashas Hebbarabailu <yashas.hebbarbail@gmail.com>
2022-05-15 11:20:28 +0530

    Added sleuth zepkin changes

--- cloud-gateway/src/main/java/com/yashas/apps/serviceregistry/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/com/yashas/apps/serviceregistry/CloudGatewayApplication.java
@@ -2,10 +2,8 @@ package com.yashas.apps.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
