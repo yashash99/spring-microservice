f5f55a82f166f1398ddd052d0bbfbda736b1d11b
Yashas Hebbarabailu <yashas.hebbarbail@gmail.com>
2022-05-15 09:51:06 +0530

    Changes to use config server

--- cloud-gateway/src/main/java/com/yashas/apps/serviceregistry/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/com/yashas/apps/serviceregistry/CloudGatewayApplication.java
@@ -2,30 +2,14 @@ package com.yashas.apps.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
-	
-	@Bean
-    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route(r -> r.path("/app-a/**")
-                		 .filters(f -> f.stripPrefix(1)) //https://github.com/spring-cloud/spring-cloud-gateway/issues/501#issuecomment-422165544
-                        .uri("lb://app-a/"))
-
-                .route(r -> r.path("/app-b/**")
-                		 .filters(f -> f.stripPrefix(1))
-                        .uri("lb://app-b/"))
-                .build();
-    }
 
 }
--- service-registry/src/main/java/com/yashas/apps/serviceregistry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/yashas/apps/serviceregistry/ServiceRegistryApplication.java
@@ -2,10 +2,14 @@ package com.yashas.apps.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
+@RefreshScope
+@RestController
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
